a418f0f9ff1928267d30cc014355efb42575fbc1
Gabriel Stines <gabestines@gmail.com>
2023-12-21 15:48:10 -0600

    Secure endpoints with form authentication

--- hub/src/test/java/org/tapsensors/hub/UiControllerTest.java
+++ hub/src/test/java/org/tapsensors/hub/UiControllerTest.java
@@ -1,10 +1,13 @@
 package org.tapsensors.hub;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -16,17 +19,21 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
-@WebMvcTest(UiController.class)
+@WebMvcTest({UiController.class, SecurityConfig.class})
 class UiControllerTest {
+
     @MockBean
     private SensorRepository sensorRepository;
 
     @Autowired
     private MockMvc mockMvc;
 
+    @BeforeEach
+    void setup(){
+        when(sensorRepository.findAllByOrderByIdAsc()).thenReturn(List.of());
+    }
     @Test
     void dashboard_notAuthenticated_isUnauthorized() throws Exception {
-        when(sensorRepository.findAllByOrderByIdAsc()).thenReturn(List.of());
         mockMvc.perform(MockMvcRequestBuilders.get("/"))
                 .andExpect(status().isUnauthorized());
     }
@@ -34,7 +41,6 @@ class UiControllerTest {
     @Test
     @WithMockUser
     void dashboard_whenAuthenticated_returns() throws Exception {
-        when(sensorRepository.findAllByOrderByIdAsc()).thenReturn(List.of());
         mockMvc.perform(MockMvcRequestBuilders.get("/"))
                 .andExpect(status().isOk());
     }
