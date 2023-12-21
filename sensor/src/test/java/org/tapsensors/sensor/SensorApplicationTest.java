8957eea0e8078909ed20dd369205a48e4467cdbe
Dariusz Lorenc <dlorenc@vmware.com>
2023-12-21 14:43:03 -0700

    Add tests ;)

--- sensor/src/test/java/org/tapsensors/sensor/SensorApplicationTest.java
+++ sensor/src/test/java/org/tapsensors/sensor/SensorApplicationTest.java
@@ -1,11 +1,9 @@
 package org.tapsensors.sensor;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Disabled
 class SensorApplicationTest {
 
 	@Test
