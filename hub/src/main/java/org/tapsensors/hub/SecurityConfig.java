a88b65495833d3bc9b7c275c22c522b0f3e9d9ba
Gabriel Stines <gabestines@gmail.com>
2023-12-22 12:00:07 -0600

    Adds login redirect url

--- hub/src/main/java/org/tapsensors/hub/SecurityConfig.java
+++ hub/src/main/java/org/tapsensors/hub/SecurityConfig.java
@@ -3,7 +3,6 @@ package org.tapsensors.hub;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,7 +14,8 @@ public class SecurityConfig {
     @Order(2)
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login(customizer -> customizer
+                        .defaultSuccessUrl("/dashboard"))
                 .authorizeHttpRequests(http -> http.requestMatchers("/readyz", "/livez", "/").permitAll())
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
 
