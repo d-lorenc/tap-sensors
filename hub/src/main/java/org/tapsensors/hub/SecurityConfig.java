a930af8292dd69f5500a2ef74f41b00aa5faf409
Gabriel Stines <gabestines@gmail.com>
2023-12-27 17:15:54 -0600

    Make JWT decoder explicitly

--- hub/src/main/java/org/tapsensors/hub/SecurityConfig.java
+++ hub/src/main/java/org/tapsensors/hub/SecurityConfig.java
@@ -1,19 +1,29 @@
 package org.tapsensors.hub;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    JwtDecoder jwtDecoder(OAuth2ClientProperties oAuth2ClientProperties){
+        String issuerUri = oAuth2ClientProperties.getProvider().get("sso").getIssuerUri();
+        System.out.println("issuer uri = " + issuerUri);
+        return JwtDecoders.fromIssuerLocation(issuerUri);
+    }
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .oauth2ResourceServer(rs -> rs.jwt(Customizer.withDefaults()))
+                .oauth2ResourceServer(resourceServer -> resourceServer.jwt(Customizer.withDefaults()))
                 .authorizeHttpRequests(http ->
                         http.requestMatchers("/readyz", "livez").permitAll()
                                 .anyRequest().authenticated());
