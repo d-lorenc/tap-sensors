5fa2fecb0b13abf35ddd9d72d35326be228ca222
Gabriel Stines <gabestines@gmail.com>
2023-12-27 16:26:36 -0600

    Uses webflux security matchers

--- gateway/src/main/java/org/tapsensors/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/org/tapsensors/gateway/config/SecurityConfig.java
@@ -3,20 +3,21 @@ package org.tapsensors.gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    SecurityWebFilterChain securityFilterChain(ServerHttpSecurity httpSecurity) {
         return httpSecurity.oauth2Login(Customizer.withDefaults())
-                .authorizeHttpRequests(http ->
-                        http.requestMatchers("/readyz", "/livez").permitAll()
-                                .anyRequest().authenticated())
+                .authorizeExchange(ex -> {
+                    ex.pathMatchers("/readyz", "/livez").permitAll();
+                    ex.anyExchange().authenticated();
+                })
                 .build();
     }
 }
