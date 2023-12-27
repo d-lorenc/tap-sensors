720170379657a0e1ef5d70bf2436dc607675400e
Gabriel Stines <gabestines@gmail.com>
2023-12-27 16:10:19 -0600

    Adds spring security to gateway

---
+++ gateway/src/main/java/org/tapsensors/gateway/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package org.tapsensors.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.oauth2Login(Customizer.withDefaults())
+                .authorizeHttpRequests(http ->
+                        http.requestMatchers("/readyz", "/livez").permitAll()
+                                .anyRequest().authenticated())
+                .build();
+    }
+}
