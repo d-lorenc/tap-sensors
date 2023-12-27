9c662c66faea100087663ebc096bead07e2cea22
Gabriel Stines <gabestines@gmail.com>
2023-12-27 15:42:04 -0600

    Allow all requests to sensor-data

--- hub/src/main/java/org/tapsensors/hub/SecurityConfig.java
+++ hub/src/main/java/org/tapsensors/hub/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.tapsensors.hub;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,13 +12,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    @Order(2)
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .oauth2Login(customizer -> customizer.defaultSuccessUrl("/dashboard", true))
-                .authorizeHttpRequests(http -> http.requestMatchers("/readyz", "/livez", "/").permitAll())
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
-
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+//                .oauth2ResourceServer(Customizer.withDefaults())
         return httpSecurity.build();
     }
 }
