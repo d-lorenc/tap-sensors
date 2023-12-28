c749a89d181c27b35150c69b081467f859a561b9
Gabriel Stines <gabestines@gmail.com>
2023-12-28 14:10:41 -0600

    Remove extraneous JWT decoder

--- hub/src/test/java/org/tapsensors/hub/TestSecurityConfig.java
+++ hub/src/test/java/org/tapsensors/hub/TestSecurityConfig.java
@@ -1,19 +1,11 @@
 package org.tapsensors.hub;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.tapsensors.hub.config.BaseSecurityConfig;
-
-import static org.mockito.Mockito.mock;
 
 @Configuration
 class TestSecurityConfig extends BaseSecurityConfig {
-    @Bean
-    JwtDecoder jwtDecoder(){
-        return mock(JwtDecoder.class);
-    }
     @Override
     public void applyOauth2ResourceServer(HttpSecurity httpSecurity) throws Exception {
     }
